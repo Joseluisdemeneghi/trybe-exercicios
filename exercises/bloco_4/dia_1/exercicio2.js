9fed4096f7bc92a26a46c4a399f0bb35ee7d47ea
José Demeneghi <josedemeneghi_cba@hotmail.com>
2021-05-19 17:41:36 -0400

    adicionando exercicios 2 e 3

---
+++ exercises/bloco_4/dia_1/exercicio2.js
@@ -0,0 +1,9 @@
+let a = 15;
+let b = 33;
+
+if (a > b) {
+  console.log (a);
+}
+else {
+  console.log (b);
+}
---
+++ exercises/bloco_4/dia_1/exercicio3.js
@@ -0,0 +1,13 @@
+let a = 50;
+let b = 40;
+let c = 30;
+
+if (a > b && a > c) {
+  console.log (a);
+}
+else if (b > a && b > c) {
+  console.log (b);
+}
+else {
+  console.log (c);
+}
