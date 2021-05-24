dba658f4254e352ad39e4228a6d3f7118cc84ff5
José Demeneghi <josedemeneghi_cba@hotmail.com>
2021-05-24 14:29:33 -0400

    adicionando exercicios - objetos e for/in

---
+++ exercises/bloco_4/dia_4/exercicio5.js
@@ -0,0 +1,22 @@
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim',
+};
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+for (let index in info) {
+  if (info[index] === info.recorrente && info.recorrente === 'Sim' && info2.recorrente === 'Sim'){
+    console.log('Ambos recorrentes');
+  }
+  else {
+    console.log(info[index]+ ' e ' + info2[index]);
+  }
+}
