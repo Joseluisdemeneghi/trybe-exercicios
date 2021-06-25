cb5d729450b8756624e9c4bd07e7440bc6edb112
José Demeneghi <josedemeneghi_cba@hotmail.com>
2021-06-25 14:50:29 -0400

    exercicio 5

--- exercises/bloco_8/dia_3/ex5.js
+++ exercises/bloco_8/dia_3/ex5.js
@@ -63,3 +63,18 @@ const books = [
   },
 ];
 
+//5 - Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia.
+
+const expectedResult = [
+  'Frank Herbert',
+  'George R. R. Martin',
+  'Isaac Asimov',
+  'J. R. R. Tolkien',
+];
+
+function fantasyOrScienceFictionAuthors() {
+  const genreAuthors = books.filter((book) => book.genre === 'Fantasia' || book.genre === 'Ficção Científica');
+  return genreAuthors.map((item) => item.author.name).sort();
+}
+
+assert.deepStrictEqual(fantasyOrScienceFictionAuthors(), expectedResult);
