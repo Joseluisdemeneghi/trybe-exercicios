7f2e9f06ffca573fc3027530a88fc3d592ee1774
José Demeneghi <josedemeneghi_cba@hotmail.com>
2021-06-28 14:08:52 -0400

    exercicio 4 resolvido

--- exercises/bloco_8/dia_4/ex4.js
+++ exercises/bloco_8/dia_4/ex4.js
@@ -63,3 +63,26 @@ const books = [
   },
 ];
 
+// 4 - Encontre o livro com o maior nome.
+
+const expectedResult = {
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  genre: 'Fantasia',
+  author: {
+    name: 'George R. R. Martin',
+    birthYear: 1948,
+  },
+  releaseYear: 1991,
+};
+
+function longestNamedBook() {
+  return books.reduce((longest, current) => {
+    if (current.name.length > longest.name.length) {
+      return current;
+    }
+    return longest;
+  })
+}
+
+assert.deepStrictEqual(longestNamedBook(), expectedResult);
