6f2fc3bca453812cf1fba79796dca24b030ba61b
José Demeneghi <josedemeneghi_cba@hotmail.com>
2021-07-07 14:07:58 -0400

    js e html ex 1

---
+++ exercises/bloco_9/dia_2/apiScript.js
@@ -0,0 +1,15 @@
+// apiScript.js     
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = () => {
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+
+  fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then(info => document.getElementById('jokeContainer').innerText = info.joke);
+};
+
+window.onload = () => fetchJoke();
