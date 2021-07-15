a505c21a0b8aee78b1c7eae55634960cbbe48860
José Demeneghi <josedemeneghi_cba@hotmail.com>
2021-07-15 15:11:32 -0400

    exercicio 4

---
+++ exercises/bloco_10/dia_3/ex4.js
@@ -0,0 +1,7 @@
+const getUpper = (string) => string.toUpperCase();
+
+const getFirst = (string) => string[0];
+
+const getConc = (str1, str2) => str1.concat(str2);
+
+module.exports = { getUpper, getFirst, getConc };
